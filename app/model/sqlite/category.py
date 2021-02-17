fbf33e4bbf38bca865e00ba3e4d1d94117eda130
Won Seo <swhors@gmail.com>
2021-02-17 17:49:24 +0900

    [DEV-0] - 커밋 되지 않은 파일 추가

---
+++ app/model/sqlite/category.py
@@ -0,0 +1,37 @@
+import json
+
+class Category:
+    _col_id_='id'
+    _col_cnt_='cnt'
+    _col_title_='title'
+    _table_name_='category'
+
+    @classmethod
+    def create_table(cls) -> str:
+        return f"create table if not exists {cls._table_name_} ( \n" +\
+            f"    {cls._col_id_} integer primary key autoincrement, \n" +\
+            f'    {cls._col_title_} text,  \n' +\
+            f'    {cls._col_cnt_} integer default 1);'
+
+    def __init__(self, id: int, title: str, cnt: int):
+        self.title = title
+        self.cnt = cnt
+        self.id = id
+
+    def __del__(self):
+        pass
+
+    def __str__(self):
+        return f'{self._col_id_}={self.id},' + \
+               f'{self._col_title_}={self.title},' + \
+               f'{self._col_cnt_}={self.cnt}'
+
+    def toJson(self):
+        return json.dumps(self.__dict__)
+
+if __name__=='__main__':
+    print(__name__)
+    print(Category.create_table())
+    print(f'Table Name = {Category._table_name_}')
+    print(Category(1, "test", 1))
+    print(Category(1, "test", 1).toJson())
---
+++ app/model/sqlite/goods_model.py
@@ -0,0 +1,94 @@
+import sqlite3
+from app.model.sqlite.goods import Goods
+from app.model.sqlite import _db_name_ as db_name
+
+
+class GoodsModel:
+    def __init__(self):
+        print("create mydata_goods")
+        self.conn = sqlite3.connect(db_name, check_same_thread=False)
+        cur = self.conn.cursor() # 2. 커서 생성 (트럭, 연결로프)
+        cur.execute(Goods.create_table())
+        cur.close()
+
+    def __del__(self):
+        print("delete mydata_goods")
+        self.conn.close()
+
+    def incCategoryCount(self, title: str):
+        sql1 =f"select * from {Goods._table_name_} where title=\'{title}\';"
+        sql2 =f"update {Goods._table_name_} set cnt=cnt + 1 \n" +\
+              f"    where title= \'{title}\'"
+        sql3 =f"insert into {Goods._table_name_}(title, cnt)\n" +\
+              f"    values(\'{title}\',1);"
+
+        cur = self.conn.cursor()
+        cur.execute(sql1)
+        rows = cur.fetchall()
+        if len(rows) > 0:
+            cur.execute(sql2)
+        else:
+            cur.execute(sql3)
+        self.conn.commit()
+        cur.close()
+
+    def getGoods(self):
+        categories = []
+        sql =f"select * from {Goods._table_name_} order by cnt desc;"
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        for row in rows:
+            if len(row) == 3:
+                categories.append(Category(row[0],row[1], row[2]))
+        cur.close()
+        return categories
+
+    def deleteGoods(self, name: str, goods_id: int, mall_name: str):
+        sql = f"delete from {Goods._table_name_} \n" +\
+              f"    where {Goods._col_name_}={name} and \n" +\
+              f"          {Goods._col_id_}={goods_id} and \n" +\
+              f"          {Goods._col_mall_name_}={mall_name};"
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        cur.close()
+
+    def deleteAll(self):
+        cur = self.conn.cursor()
+        cur.execute(f'delete all from {Goods._table_name_};')
+        cur.close()
+
+    def insertGoods(self, name: str, goods_id: int,
+                    goods_url: str, image_url: str,
+                    mall_name: str, lprice: int,
+                    hprice: int, updated: int):
+        sql1 =f"select * from {Goods._table_name_} \n" +\
+              f"    where {Goods._col_goods_id_}={goods_id};"
+        sql2 =f"insert into {Goods._table_name_} \n" +\
+              f"    values(\'{name}\',\n" +\
+              f"           {goods_id},\n" +\
+              f"           \'{goods_url}\',\n" +\
+              f"           \'{image_url}\',\n" +\
+              f"           \'{mall_name}\',\n" +\
+              f"           {lprice},\n" +\
+              f"           {hprice},\n" +\
+              f"           {updated});"
+        cur = self.conn.cursor()
+        cur.execute(sql1)
+        rows = cur.fetchall()
+        if len(rows) <= 0:
+            cur.execute(sql2)
+            self.conn.commit()
+        cur.close()
+
+if __name__=="__main__":
+    goods = GoodsModel()
+    title: str = input("Type something to test this ( to exit type exit): ")
+    while title != 'exit':
+        goods.incCategoryCount(title)
+        goods.incCategoryCount(title)
+        goods.incCategoryCount(title)
+        goods = category.getCategories()
+        for ca in categories:
+            print(ca)
+        title: str = input("Type something to test this ( to exit type exit): ")
