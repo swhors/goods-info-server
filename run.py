b5a6f3fb12bca19bc1d34c897fe0586ac8619639
Won Seo <swhors@gmail.com>
2021-02-17 17:48:06 +0900

    [DEV-0] - SSL 관련 수정 작업 중

--- run.py
+++ run.py
@@ -2,8 +2,10 @@ from app import app
 import ssl
 #main
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-ssl_context.load_cert_chain(certfile='simpson.crt', keyfile='private.key', password='aq1234')
-ssl_context_1=('simpson.crt', 'private.key')
+ssl_context.load_cert_chain(certfile='simpson.crt',
+                            keyfile='private.key',
+                            password='aq1234')
+ssl_context_1=('ssh\simpson.crt', 'ssh\private.key')
 app.run (host='0.0.0.0',
          debug=True,
          ssl_context=ssl_context,
