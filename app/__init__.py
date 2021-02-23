2b2f53627c20070778ddf34deb47bfc084ab7fb5
Won Seo <swhors@gmail.com>
2021-02-23 18:19:20 +0900

    [DEV-0] - 수정중

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ import os
 
 from flask import Flask
 from flask_login import LoginManager
-from flask_jwt import JWT, jwt_required
+# from flask_jwt import JWT, jwt_required
+from flask_jwt_extended import (
+    JWTManager, jwt_required, jwt_optional, create_access_token, get_jwt_identity, get_jwt_claims)
 
 from app.model.user import User, USERS
 
@@ -12,6 +14,8 @@ from app.ctl.goods_ctl import goods_ctl
 from app.ctl.category_ctl import category_ctl
 from app.ctl.loginout_ctl import loginout_ctl
 from app.service.user_service import UserService
+
+from config import Config
 
 
 app=Flask(__name__)
@@ -33,15 +37,17 @@ def user_loader(user_id) -> User:
     ret, user = user_service.get_user(user_id)
     return user
 
-def authenticate(username, password):
-    ret, user = user_service.get_user(user_id)
+# def authenticate(username, password):
+#     ret, user = user_service.get_user(user_id)
+#
+#     if user and safe_str_cmp(user.password, password):
+#         return user
+#
+# def identity(payload):
+#     user_id = payload['identity']
+#     ret, user = user_service.get_user(user_id)
+#     return user
 
-    if user and safe_str_cmp(user.password, password):
-        return user
-
-def identity(payload):
-    user_id = payload['identity']
-    ret, user = user_service.get_user(user_id)
-    return user
-
-jwt = JWT(app, authenticate, identity)
+# jwt = JWT(app, authenticate, identity)
+app.config['JWT_SECRET_KEY'] = Config.jwt_key
+jwt = JWTManager(app)
